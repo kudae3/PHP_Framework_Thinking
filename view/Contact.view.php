e389c8c86d33a15b3c8dc187dc44945c75efb691
Kudae Sithu <kudaesithu2@gmail.com>
2024-12-16 13:35:11 +0630

    Refactored Some Codes

--- view/Contact.view.php
+++ view/Contact.view.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php require "Partials/header.php" ?>
     <h3>Contact Page</h3>
-</body>
-</html>
+<?php require "Partials/footer.php" ?>
